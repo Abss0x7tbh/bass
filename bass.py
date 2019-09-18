3cf72c99306f8fb352f18c33244c5a7c3e246037
Shuaib Oladigbolu <saoladigbolu@gmail.com>
2019-09-18 18:01:41 +0100

    Final refactor

--- bass.py
+++ bass.py
@@ -21,7 +21,7 @@ print("\n\n")
 ap = argparse.ArgumentParser()
 ap.add_argument(
     "-d", "--domain", required=True,
-	help="target domain"
+    help="target domain"
 )
 ap.add_argument(
     "-o", "--output", required=True,
@@ -30,8 +30,8 @@ ap.add_argument(
 args = vars(ap.parse_args())
 
 # global scope
-domain=args["domain"]
-output=args["output"]
+domain = args["domain"]
+output = args["output"]
 providers = []
 
 
@@ -39,7 +39,7 @@ providers = []
 def setProv():
 
     try:
-        answers = dns.resolver.query(domain,'NS')
+        answers = dns.resolver.query(domain, 'NS')
     except dns.exception.DNSException:
         print("Domain failed to resolve")
         sys.exit(1)
@@ -51,7 +51,6 @@ def setProv():
         ext = tldextract.extract(str(server.target).rstrip('.'))
         # extensions matter on Win
         providers.append('./resolvers/'+ext.domain+'.txt')
-
 
     # set to remove duplicate
     final_providers = list(set(providers))
@@ -67,31 +66,35 @@ def setProv():
 
 # join multiple resolvers from different provider txt files
 def createFinal(fp):
-    outfile=open(output,"w")
+    outfile = open(output, "w")
     for fname in fp:
         try:
             with open(fname) as infile:
                 for line in infile:
                     outfile.write(line)
         except IOError:
-            print("Provider "+fname+" not available. Add an issue with the name of the provider so that we can look into it")
+            print("Provider "+fname+" not available. Add an issue with the\
+                 name of the provider so that we can look into it")
     outfile.close()
+
 
 # display output
 def displayList():
     f = open(output, 'r')
     file_contents = f.read()
-    print(Fore.GREEN+ file_contents)
+    print(Fore.GREEN + file_contents)
     f.close()
+
 
 # number of total resolvers that can be used
 def displayStats():
     num_lines = sum(1 for line in open(output))
     return num_lines
 
+
 def main():
 
-    fp=setProv()
+    fp = setProv()
     createFinal(fp)
     print(Fore.GREEN+"Final List of Resolver located at "+output+" :")
     print("-----------------------------")
@@ -100,11 +103,10 @@ def main():
     print("\n")
     print(Fore.RED+"Stats")
     print("-----------------------------")
-    num=displayStats()
+    num = displayStats()
     print(Fore.GREEN+'Total usable resolvers : '+Fore.RED+str(num))
     print(Fore.RED+"-----------------------------")
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
-
